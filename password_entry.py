4451ec7f843431048e52a9b3ba2fe6114422bbeb
kyawphyoaung <aprilnikee@gmail.com>
2019-08-07 21:11:16 +0800

    Add password checker

--- password_entry.py
+++ password_entry.py
@@ -2,3 +2,19 @@
 Name : Kyaw Phyo Aung
 JCU ID : 13822414
 """
+MINIMUM_LENGTH = 6
+PASSWORD = "Pythonista"
+length_count = 0
+user_pwd = input("Enter the password: " )
+
+if len(user_pwd) >= MINIMUM_LENGTH :
+    if user_pwd == PASSWORD :
+        print("Your enterd password is :")
+        while length_count <= len(user_pwd):
+            print("*",end="")
+            length_count += 1
+        print("\n Correct!")
+    else:
+        print("Wrong!")
+else:
+    print("Minimum password length is 6")

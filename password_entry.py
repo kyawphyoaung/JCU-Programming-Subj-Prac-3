267597de1c6cdd8132673688c1aab474b2da572f
kyawphyoaung <aprilnikee@gmail.com>
2019-08-11 14:32:10 +0800

    Refactor password program to use functions

--- password_entry.py
+++ password_entry.py
@@ -4,17 +4,31 @@ JCU ID : 13822414
 """
 MINIMUM_LENGTH = 6
 PASSWORD = "Pythonista"
-length_count = 0
-user_pwd = input("Enter the password: " )
 
-if len(user_pwd) >= MINIMUM_LENGTH :
-    if user_pwd == PASSWORD :
-        print("Your enterd password is :")
-        while length_count <= len(user_pwd):
-            print("*",end="")
-            length_count += 1
-        print("\n Correct!")
+def main():
+    length_count = 0
+    user_pwd = get_password()
+
+    pass_fun(length_count, user_pwd)
+
+
+def pass_fun(length_count, user_pwd):
+    if len(user_pwd) >= MINIMUM_LENGTH:
+        if user_pwd == PASSWORD:
+            print("Your enterd password is :")
+            while length_count <= len(user_pwd):
+                print("*", end="")
+                length_count += 1
+            print("\n Correct!")
+        else:
+            print("Wrong!")
     else:
-        print("Wrong!")
-else:
-    print("Minimum password length is 6")
+        print("Minimum password length is 6")
+
+
+def get_password():
+    user_pwd = input("Enter the password: ")
+    return user_pwd
+
+
+main()
